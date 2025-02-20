0e67e848136b2db39092deb2ba590caa49c455fd
anangj <anangjulyanto257@gmail.com>
2025-02-20 15:36:13 +0700

    add model admin

---
+++ app/models/Admin.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const AdminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["admin", "super_admin"],
+    default: "admin",
+    required: true
+  },
+  permissions: {
+    type: [String],
+    enum: ["approve_membership", "resend_e_card", "view_reports"],
+    default: []
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("Admin", AdminSchema);
